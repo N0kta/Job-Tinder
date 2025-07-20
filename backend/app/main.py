32ec32a9695f0236320b206443840c996e9b7ef4
Serhat Marangoz <serhatmar05@gmail.com>
2025-07-20 21:01:58 +0200

    Work on FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,16 @@ from routers.jobs import router as jobs_router
 from routers.users import router as users_router
 from routers.auth import router as auth_router
 
+from pydantic import BaseModel
+
+class NameRequest(BaseModel):
+    name: str
+
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "ey du bist am root"}
 
 app.include_router(jobs_router, prefix="/jobs", tags=["Jobs"])
 app.include_router(users_router, prefix="/users", tags=["Users"])
